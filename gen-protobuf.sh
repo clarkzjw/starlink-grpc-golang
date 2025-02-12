159b0d1869b9b55bac52e82166674b6141a149c8
Jinwei Zhao <clarkzjw@uvic.ca>
2025-02-12 10:50:55 -0800

    add 2025.02.05.mr49087 / d1223ab6-7fb9-490d-affc-dea80a6be413.uterm_manifest.release

--- gen-protobuf.sh
+++ gen-protobuf.sh
@@ -8,6 +8,8 @@ echo "$dish_version"
 
 protoset_file=protoset/"$dish_version"/dish.protoset
 
+protoc --go_out=./pkg/ --go-grpc_out=./pkg/ --descriptor_set_in="$protoset_file" spacex/api/device/rssi_scan.proto
+
 protoc --go_out=./pkg/ --go-grpc_out=./pkg/ --descriptor_set_in="$protoset_file" spacex/api/common/status/status.proto
 protoc --go_out=./pkg/ --go-grpc_out=./pkg/ --descriptor_set_in="$protoset_file" spacex/api/device/command.proto
 protoc --go_out=./pkg/ --go-grpc_out=./pkg/ --descriptor_set_in="$protoset_file" spacex/api/device/common.proto
@@ -66,6 +68,8 @@ protoc --go_out=./pkg/  --go-grpc_out=./pkg/ \
 protoc --go_out=./pkg/  --go-grpc_out=./pkg/ \
        --go_opt=Mspacex/api/telemetron/public/common/time.proto=spacex.com/api/telemetron/public/common/time \
        --go-grpc_opt=Mspacex/api/telemetron/public/common/time.proto=spacex.com/api/telemetron/public/common/time \
+       --go_opt=Mspacex/api/satellites/network/ut_disablement_codes.proto=spacex.com/api/satellites/network/ut_disablement_codes \
+       --go-grpc_opt=Mspacex/api/satellites/network/ut_disablement_codes.proto=spacex.com/api/satellites/network/ut_disablement_codes \
        --descriptor_set_in="$protoset_file" \
        spacex/api/device/wifi.proto
 
