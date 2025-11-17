577dbfe25549391bf7a4b48c147b34a58fb632fd
Jinwei Zhao <clarkzjw@uvic.ca>
2025-11-16 23:14:34 -0800

    add decoded proto

--- get-protoset.sh
+++ get-protoset.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 # requires https://github.com/fullstorydev/grpcurl
-
+# protodump requires:
+#   go install github.com/arkadiyt/protodump/cmd/protodump@latest
 grpc_endpoint="192.168.100.1:9200"
-#grpc_endpoint="127.0.0.1:1234"
 
 dish_version=$(grpcurl -plaintext -d {\"get_status\":{}} $grpc_endpoint SpaceX.API.Device.Device/Handle | grep softwareVersion | awk '{print $2}' | awk -F',' '{print $1}' | awk -F'\"' '{print $2}')
 echo "$dish_version" > VERSION
 mkdir -p "protoset/$dish_version"
+mkdir -p "proto/"
 
 # get dish protoset
 grpcurl -plaintext -protoset-out protoset/"$dish_version"/dish.protoset $grpc_endpoint describe SpaceX.API.Device.Device
 
 protoc --decode_raw < protoset/"$dish_version"/dish.protoset | grep "proto\"$" | awk '{print $2}' | awk -F\" '{print $2}' | grep spacex | sort | uniq > protoset/"$dish_version"/VERSION
+
+protodump -file protoset/"$dish_version"/dish.protoset -output "proto/"
